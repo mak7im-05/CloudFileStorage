fd02b60964f26fc9a190af4f535a490bd9f3d29d
Maxim <karimovmaksim121@gmail.com>
2025-02-04 06:38:38 +0300

    Добавлен: Liquibase

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,8 @@
+--liquibase formatted sql
+
+--changeset maxim:1
+CREATE TABLE  person (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
